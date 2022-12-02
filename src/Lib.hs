4b5b6fc7dd15e8597245df6837008c009ae45107
MOKTAR ALHASAPI <moktaralhasapi@gmail.com>
2022-12-02 20:52:20 +0100

    first passed ...

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,20 @@ module Lib where
 
 main :: IO ()
 main =
-  zip [1..] . (sum <$>) . mtakeW [] [] . lines <$> readFile "input.txt" >>= mapM_ print
+  fst
+  . maxDeer
+  . zip [1..]
+  . (sum <$>)
+  . mtakeW [] []
+  . lines <$> readFile "input.txt" >>= print
   where
-    mtakeW acc _    [] = acc
+    mtakeW acc _    []      = acc
     mtakeW acc acc' ("":xs) = mtakeW (acc ++ [(read :: String -> Integer) <$> acc']) [] xs
     mtakeW acc acc' (x:xs)  = mtakeW acc (x:acc') xs
 
+    maxDeer :: [(Integer, Integer)] -> (Integer, Integer)
+    maxDeer = foldl1 predicate
+
+    predicate :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
+    predicate (idx, valx) (idy, valy) | valx > valy = (idx, valx)
+                                      | otherwise   = (idy, valy)
